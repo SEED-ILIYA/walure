9ca9de01f06afd49624eb5d18fd50523dd87fb0e
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-10-04 18:34:44 +0100

    Add project files.

---
+++ Csharp-Introduction/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Console.WriteLine("Hello, World!");
+Console.WriteLine("Hey Beautiful people");
+Console.WriteLine("hello");*/
+//MyMethod();
+
+
+////method
+//static void MyMethod()
+//{
+//    Console.WriteLine("welcome to the academy!");
+//}
+
+
+//datatypes
+//string
+//integer int
+//float
+//double
+//decimal
+//boolean bool
+//char
+//byte
+//long
+//datetime
+
+//datatype      //variable              //value
+string           word               = " welcome back";
+Console.WriteLine(word);
+
+int number = 679988989;
+float num = 93291238485693278.756f;
+double n = 32174187463871849.236164144372173;
+decimal y = 23167451647163464.845362m;
+bool x = true;
+char w = 'a';
+long r = 3456521383783281744;
+byte l = 99;
+DateTime dt = DateTime.Now;
+Console.WriteLine(dt);
+
+
+
+
+
+
+
+
+
+
+
+
+
