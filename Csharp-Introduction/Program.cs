795f5d83f0f10cc4bd4c275259fe92ac2362f912
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-10-11 18:11:20 +0100

    sequence

--- Csharp-Introduction/Program.cs
+++ Csharp-Introduction/Program.cs
@@ -1,46 +1,17 @@
-﻿//statement
-//conditional statement
-//conditional looping
+﻿//escape sequence
+//\'
+//\"
+//\
+//\n
+//\t
+//\0
+//\b
 
-//conditional statement 
-//if/else
-//switch case
-
-
-//conditional looping
-//for
-//foreach
-//while
-//dowhile
-
-
-//for
-    //initializer, condition, iterator
-//for(int i = 0; i<10; i++)
-//{
-//    Console.WriteLine(i);
-//}
-
-int[] number = { 5, 6, 7, 8, 9, 0, 10, 2, 3, 3, 4, 4 };
-string[] Words = { "chalk", "board", "pen", "bin", "gate" };
-//foreach
-foreach(string x in Words)
-{
-    Console.WriteLine(x);
-}
-
-//while
-int i = 0;
-while (i < 5)
-{
-    Console.WriteLine(i);
-    i++;
-}
-
-//do while
-int y = 0;
-do
-{
-    Console.WriteLine(y);
-    y+=2;
-}while(y<10);
+string word = "\tPlan \n Execute \n Don't give up";
+Console.WriteLine(word);
+string result = "please \" can you see it \"";
+Console.WriteLine(result);
+string value = "\bvalue";
+Console.WriteLine(value);
+string v = "c:\\web\\page\\file.cs";
+Console.WriteLine(v);
