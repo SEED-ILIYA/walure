7b09af1db8d7a3c84d64b35f307fc12f540c9de8
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-11-01 17:47:08 +0100

    stack&queue

--- Csharp-Introduction/Program.cs
+++ Csharp-Introduction/Program.cs
@@ -1,15 +1,96 @@
-﻿//Dictionary
-Dictionary<string, int> infodetails  = new Dictionary<string, int>();
-infodetails.Add("John", 89);
-infodetails.Add("Ben", 59);
-infodetails.Add("Serah", 9);
-infodetails.Add("Todd", 8);
-infodetails.Add("Ken", 91);
-infodetails.Add("grace", 19);
-infodetails.Add("Shola", 29);
+﻿//stack&queue
+//queue = first-in first-out
 
+using Csharp_Introduction;
 
-foreach(var info in infodetails)
+Student stud1 = new Student()
 {
-    Console.WriteLine("key: {0}, Value:{1}",info.Key, info.Value);
-}
+    Gender = "Male",
+    Id = 1,
+    Name = "Ben"
+};
+
+Student stud2 = new Student()
+{
+    Gender = "Male",
+    Id = 2,
+    Name = "Shola"
+};
+
+Student stud3 = new Student()
+{
+    Gender = "Female",
+    Id = 3,
+    Name = "Joy"
+};
+
+Student stud4 = new Student()
+{
+    Gender = "Female",
+    Id = 4,
+    Name = "Grace"
+};
+
+Student stud5 = new Student()
+{
+    Gender = "Male",
+    Id = 5,
+    Name = "Todd"
+};
+
+
+//stack = last-in, first-out 
+
+Stack<Student> stackstud = new Stack<Student>();
+stackstud.Push(stud1);
+stackstud.Push(stud2);
+stackstud.Push(stud3);
+stackstud.Push(stud4);
+stackstud.Push(stud5);
+
+//Student std = stackstud.Pop();
+//Console.WriteLine(std.Name + " - " + std.Gender);
+//Console.WriteLine("total item in the queue list = " + stackstud.Count());
+
+//Student std2 = stackstud.Pop();
+//Console.WriteLine(std2.Name + " - " + std2.Gender);
+//Console.WriteLine("total item in the queue list = " + stackstud.Count());
+Student std2 = stackstud.Peek();
+Console.WriteLine(std2.Name + " - " + std2.Gender);
+Console.WriteLine("total item in the queue list = " + stackstud.Count());
+
+
+
+
+////queue
+//Queue<Student> queuestud = new Queue<Student>();
+//queuestud.Enqueue(stud1);
+//queuestud.Enqueue(stud2);
+//queuestud.Enqueue(stud3);
+//queuestud.Enqueue(stud4);
+//queuestud.Enqueue(stud5);
+
+
+//Student std = queuestud.Dequeue();
+//Console.WriteLine(std.Name + " - " + std.Gender);
+//Console.WriteLine("total item in the queue list = " + queuestud.Count());
+//Student std2 = queuestud.Dequeue();
+//Console.WriteLine(std2.Name + " - " + std2.Gender);
+//Console.WriteLine("total item in the queue list = " + queuestud.Count());
+//Student std3 = queuestud.Dequeue();
+//Console.WriteLine(std3.Name + " - " + std3.Gender);
+//Console.WriteLine("total item in the queue list = " + queuestud.Count());
+//Student std4 = queuestud.Dequeue();
+//Console.WriteLine(std4.Name + " - " + std4.Gender);
+//Console.WriteLine("total item in the queue list = " + queuestud.Count());
+
+//foreach(Student studs in queuestud)
+//{
+//    Console.WriteLine(studs.Name + " - " + studs.Gender);
+//    Console.WriteLine("total item in the queue list = " + queuestud.Count());
+
+//}
+
+//Student std4 = queuestud.Peek();
+//Console.WriteLine(std4.Name + " - " + std4.Gender);
+//Console.WriteLine("total item in the queue list = " + queuestud.Count());
--- Csharp-Introduction/Student.cs
+++ Csharp-Introduction/Student.cs
@@ -7,35 +7,11 @@ using System.Threading.Tasks;
 
 namespace Csharp_Introduction
 {
-    /// <summary>
-    /// this clas is a static class and it has extension method in it
-    /// </summary>
-    public static class Student
+    public class Student
     {
-        /// <summary>
-        /// this method is an extension that check for interger variables
-        /// </summary>
-        /// <param name="i"></param>
-        /// <param name="i2"></param>
-        /// <returns>bool</returns>
-        public static bool IsGreater(this int i, int i2)
-        {
-            return i > i2;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public static void MyMethod()
-        {
-
-        }
-        #region this method is to be refactored later
-        public static void MyMethod2()
-        {
-            Console.WriteLine("Hello World");
-        }
-        #endregion
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
     }
 
 
